c8aca1659b3c638c3b5600f0fb42f478ed7301df
bockyang <bockyanggoh@gmail.com>
2025-04-20 00:05:55 +0800

    fixed bug where minor and patch were not reset to 0

--- src/generator/generated_version.go
+++ src/generator/generated_version.go
@@ -8,9 +8,9 @@ import (
 )
 
 type GeneratedVersion struct {
-	Major *string
-	Minor *string
-	Patch *string
+	Major string
+	Minor string
+	Patch string
 }
 
 func GetGeneratedVersion(version string) (*GeneratedVersion, error) {
@@ -21,51 +21,54 @@ func GetGeneratedVersion(version string) (*GeneratedVersion, error) {
 		return nil, errors.New("Version string does not fit semver structure")
 	}
 	return &GeneratedVersion{
-		Major: &secondSplit[0],
-		Minor: &secondSplit[1],
-		Patch: &secondSplit[2],
+		Major: secondSplit[0],
+		Minor: secondSplit[1],
+		Patch: secondSplit[2],
 	}, nil
 }
 
 func (r *GeneratedVersion) IncrementPatch() error {
-	intPatch, err := strconv.Atoi(*r.Patch)
+	intPatch, err := strconv.Atoi(r.Patch)
 	if err != nil {
 		return err
 	}
 	intPatch = intPatch + 1
 	strPatch := strconv.Itoa(intPatch)
-	r.Patch = &strPatch
+	r.Patch = strPatch
 	return nil
 }
 
 func (r *GeneratedVersion) IncrementMinor() error {
-	intMinor, err := strconv.Atoi(*r.Minor)
+	intMinor, err := strconv.Atoi(r.Minor)
 	if err != nil {
 		return err
 	}
 	intMinor = intMinor + 1
 	strMinor := strconv.Itoa(intMinor)
-	r.Minor = &strMinor
+	r.Minor = strMinor
+	r.Patch = "0"
 	return nil
 }
 
 func (r *GeneratedVersion) IncrementMajor() error {
-	intMajor, err := strconv.Atoi(*r.Major)
+	intMajor, err := strconv.Atoi(r.Major)
 	if err != nil {
 		return err
 	}
 	intMajor = intMajor + 1
 	strMajor := strconv.Itoa(intMajor)
-	r.Major = &strMajor
+	r.Major = strMajor
+	r.Minor = "0"
+	r.Patch = "0"
 	return nil
 }
 
 func (r *GeneratedVersion) BuildReleaseVersion() string {
-	version := fmt.Sprintf("%s.%s.%s", *r.Major, *r.Minor, *r.Patch)
+	version := fmt.Sprintf("%s.%s.%s", r.Major, r.Minor, r.Patch)
 	return version
 }
 
 func (r *GeneratedVersion) BuildReleaseCandidateVersion(uniqueId string) string {
-	version := fmt.Sprintf("%s.%s.%s-rc.%s", *r.Major, *r.Minor, *r.Patch, uniqueId)
+	version := fmt.Sprintf("%s.%s.%s-rc.%s", r.Major, r.Minor, r.Patch, uniqueId)
 	return version
 }
